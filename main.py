77be430913edd87c737f50c0ffa124c863aa1cc1
Norelle <126525426+ntipoai@users.noreply.github.com>
2023-03-22 22:14:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def main():
     ##################################################
     pass
 
+main(): celsius = int(input('Enter the Celcius Degree: '))
+    farenheit = (9.0 / 5.0) * celcius + 32 
+    print(f'The Farenheit is {farenheit:2f}') 
 
 if __name__ == '__main__':
     main()
